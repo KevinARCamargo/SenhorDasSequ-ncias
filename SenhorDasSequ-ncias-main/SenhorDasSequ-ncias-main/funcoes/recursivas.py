a3998a1b27fbefc89bb337f103f296f67e090bcc
Gabriel Angelo Souza de Oliveira <127458162+GabrielAngelo035@users.noreply.github.com>
2024-08-11 13:34:46 -0300

    Mudanças na lógica do jogo
    Agora jogadores respondem a mesma pergunta primeiro e depois é exibida a resposta da mesma , assim por diante até o fim do game

---
+++ SenhorDasSequ-ncias-main/SenhorDasSequ-ncias-main/funcoes/recursivas.py
@@ -0,0 +1,63 @@
+#Funções
+def sucessora(n):
+    return n + 1
+
+def ant(x):
+    if x == 0:
+        return 0
+    else:
+        return x - 1
+
+def soma(x, y):
+    if y == 0:
+        return x
+    else:
+        return sucessora(soma(x, y - 1))
+
+def subt(x, y):
+    if y == 0:
+        return x
+    else:
+        return ant(subt(x, y - 1))
+
+def mult(x, y):
+    if y == 0:
+        return 0
+    else:
+        return soma(x, mult(x, y - 1))
+    
+def elevado(x, y):
+    if y == 0:
+        return 1
+    else:
+        return mult(x, elevado(x, y - 1))
+
+def fatorial(x):
+    if x == 0:
+        return 1
+    else:
+        return mult(x, fatorial(x - 1))
+
+def div_chao(dividendo, divisor):
+    if dividendo < divisor:
+        return 0
+    else:
+        return 1 + div_chao(subt(dividendo, divisor), divisor)
+
+def div_teto(dividendo, divisor):
+    if divisor > dividendo:
+        return 0
+    if dividendo < divisor:
+        return 1
+    else:
+        return 1 + div_teto(subt(dividendo, divisor), divisor)
+
+def percentual(x, y):
+    produto = mult(x, y)
+    return div_chao(produto, 100)
+
+def resto(x, y):
+    if x < y:
+        return x
+    else:
+        return resto(subt(x, y), y)
